337a1e119f8fb531e6d7b3cb1eb1d24f29fd0268
Israel755-git <israel.755.sistemas@gmail.com>
2025-10-07 16:44:03 -0600

    ci: cambios en el script de despliegue

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+docker stop buro-laboral-backend
+docker rm buro-laboral-backend
+
 git pull --no-edit
 
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+# BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$BRANCH_NAME" == "main" ]; then
-    CONTAINER_NAME="buro-laboral-backend"
-else
-    CONTAINER_NAME="buro-laboral-backend-$BRANCH_NAME"
-fi
+CONTAINER_NAME="buro-laboral-backend"
 
 export COMPOSE_BAKE=true
 export CONTAINER_NAME
